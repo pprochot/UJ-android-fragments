49ca79a554d38b600c561ad3a088f8ed44a72d1e
pawel <pprochot16@gmail.com>
2021-12-30 21:03:50 +0100

    Add fragments

---
+++ app/src/main/java/com/github/pprochot/uj/android/fragments/App.kt
@@ -0,0 +1,9 @@
+package com.github.pprochot.uj.android.fragments
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+}
---
+++ app/src/main/java/com/github/pprochot/uj/android/fragments/activities/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.github.pprochot.uj.android.fragments.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import com.github.pprochot.uj.android.fragments.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginActivity : AppCompatActivity(R.layout.activity_login) {
+}
